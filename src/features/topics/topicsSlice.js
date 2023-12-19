9663bcb06d6b3fa5c9bd1d20beaa186fb0029526
emilchr <98517843+emilchr@users.noreply.github.com01>
2023-12-19 14:26:55 +0100

    step 10

--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -16,11 +16,18 @@ export const topicsSlice = createSlice({
                 icon,
                 quizIds: []
             }
+        },
+        addQuiz: (state, action) => {
+            const { id, name, topicId, cardIds } = action.payload;
+            state.topics[topicId].quizIds.push(id);
         }
     }
 })
 
+
+export const { addTopic, addQuiz } = topicsSlice.actions;
+
 // SELECTORS
 export const selectTopics = (state) => state.topics.topics; // Selects topics nested inside initialState
-export const { addTopic } = topicsSlice.actions;
+
 export const topicsReducer = topicsSlice.reducer;
